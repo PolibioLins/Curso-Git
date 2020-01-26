f733b898ede675d065cc9b72a5ea076469c7ae3f
Polibio Lins <polibio.l.costa@gmail.com>
2020-01-26 10:26:32 -0300

    Adicionado uma mensagem de bom dia

--- 1 - Seção 8 - Criar Repositorio Git/Program.cs
+++ 1 - Seção 8 - Criar Repositorio Git/Program.cs
@@ -7,6 +7,8 @@ namespace _1___Seção_8___Criar_Repositorio_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.Read();
         }
     }
 }
