12df2b47019ae652c2e82c636669d9869a0a995f
Polibio Lins <polibio.l.costa@gmail.com>
2020-01-26 10:39:07 -0300

    Adicionado a mensagem como você esta hoje

--- 1 - Seção 8 - Criar Repositorio Git/Program.cs
+++ 1 - Seção 8 - Criar Repositorio Git/Program.cs
@@ -8,6 +8,7 @@ namespace _1___Seção_8___Criar_Repositorio_Git
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
             Console.Read();
         }
     }
