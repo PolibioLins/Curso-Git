f5eab53238a0878728b58b45895dfe1a73934491
Polibio Lins <polibio.l.costa@gmail.com>
2020-01-26 10:24:52 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ 1 - Seção 8 - Criar Repositorio Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1___Seção_8___Criar_Repositorio_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
